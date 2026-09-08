216ad03e82d289487db2f6df4790c5cbf05f819d
Rithwik Vallabhan TV <65836706+xp4t@users.noreply.github.com>
2026-09-08 15:20:00 +0530

    Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-import re
-_YOSYS_LATCH = re.compile(r"(?<!no )latch inferred|inferring latch", re.IGNORECASE)
-print(bool(_YOSYS_LATCH.search("No latch inferred")))
-print(bool(_YOSYS_LATCH.search("latch inferred")))
